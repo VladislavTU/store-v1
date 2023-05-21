ff1ba7a2e10c7477f28e4f9d18c5d8e0b2f5a745
VladislavTU <timofeevvladislavu@gmail.rom>
2023-05-21 20:54:04 +0400

    global 0521

--- src/Components/Account.js
+++ src/Components/Account.js
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+import styles from './account.module.css';
+
 const Account = () => {
-  return <h2>User</h2>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div className="container">
+      <div className={styles.logIn}>
+        <h2>Log in</h2>
+        <form className={styles.logInForm}>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit">Continue</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Account;
